5aa1c8a9666355719d135d6830e65dde29d91dcc
hperez1123 <hector.a.perez@outlook.com>
2020-07-10 21:49:42 -0400

    Update to Java Easy1

--- Java/Easy/printHello.java
+++ Java/Easy/printHello.java
@@ -5,8 +5,23 @@
 public class Solution {
 
   public static void main(String[] args) {
-      System.out.println("Hello, World.");
-      System.out.println("Hello, Java.");
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
   }
 }
 
+// While it was correct on HackerRank, I have to note that running
+// this program is a bit challenging, seeing as I have to correct
+// the system variables, edit/add a variable to allow the Java compiler
+// to work it's magic.
+
+/*
+ * Breaking it down, I see that in Java, a class must be called. I understand
+ * that it can either be public, private, or protected. after making designating
+ * the object's class type, we call it Solution.
+ * 
+ * we open start {} after Solution. Inside is [public static void main(String[]
+ * args)], where main() is the method of the program. public static void is the
+ * method's modifiers. Static helps with memory and Void helps with . (String[]
+ * args) designates that incoming argument are strings.
+ */
